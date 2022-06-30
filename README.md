4758b013a8233d9f1c97d402de6ed0878d144080
Hossein Moaser <moaser.hossein@gmail.com>
2022-07-01 02:35:16 +0430

    Intial Commit And Layout Initialization

---
+++ README.md
@@ -0,0 +1,4 @@
+A WPF MVVM project for loading shape file and displaying on bing map.
+Note: Copy "ShapeFiles" folder to root of project.
+Hossein Moaser
+Gmail: moaser.hossein@gmail.com
---
+++ ShapeFileLoadingMVVM/ShapeFileLoading.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShapeFileLoading.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShapeFileLoading.Domain")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dda13cd9-ab66-45c2-9b94-f72411b5d789")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
